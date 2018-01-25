23699f4203f4abd1e202c5d73766d32ceb97df2b
Srisri Gokanapudy <sgoka001@ucr.edu>
2018-01-25 13:51:42 -0800

    basic layouts for login and registration

--- app/src/main/java/com/example/srisri/doodle/CustomRegistration.java
+++ app/src/main/java/com/example/srisri/doodle/CustomRegistration.java
@@ -3,13 +3,11 @@ package com.example.srisri.doodle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class CustomRegistration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_custom_registration);
     }
-
-    //hello
 }
