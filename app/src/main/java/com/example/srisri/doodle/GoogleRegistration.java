553b835e3118b04d9e7b92afa524d544a8fce3a1
Kristen Jue <kjue001@ucr.edu>
2018-01-27 16:08:28 -0800

    google sign-in part 2

---
+++ app/src/main/java/com/example/srisri/doodle/GoogleRegistration.java
@@ -0,0 +1,21 @@
+package com.example.srisri.doodle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class GoogleRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_registration);
+        GoogleSignInOptions gsin = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this,gsin);
+
+    }
+}
