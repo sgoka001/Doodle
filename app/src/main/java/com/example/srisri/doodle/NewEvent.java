7b2c157532b39df20c87b1746466cf552927f2b8
TrevorSalazar <tsala001@ucr.edu>
2018-01-29 07:10:59 -0800

    Adding setup for others parts of activity: location page, options page, settings page

--- app/src/main/java/com/example/srisri/doodle/NewEvent.java
+++ app/src/main/java/com/example/srisri/doodle/NewEvent.java
@@ -1,8 +1,45 @@
 package com.example.srisri.doodle;
 
-/**
- * Created by Salazar Family on 1/29/2018.
- */
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class NewEvent {
+public class NewEvent extends AppCompatActivity {
+
+    @Override
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+        // Setting event buttons and text boxes
+
+        Button evAddOptions = findViewById(R.id.button2);
+        evAddOptions.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                //New intent will be made here (options page)
+            }
+        });
+
+        Button evLocation = findViewById(R.id.button3);
+        evLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent for location page
+            }
+        });
+
+        Button evSettings = findViewById(R.id.button4);
+        evSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent for settings page
+            }
+        });
+
+    }
 }
