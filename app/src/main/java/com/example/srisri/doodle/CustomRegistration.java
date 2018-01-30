d531c0b2289387bde9d84790074f685f652882d0
pgior001 <pgior001@ucr.edu>
2018-01-30 15:57:07 -0800

    registration work
    registration page working with no error check/messages

--- app/src/main/java/com/example/srisri/doodle/CustomRegistration.java
+++ app/src/main/java/com/example/srisri/doodle/CustomRegistration.java
@@ -3,6 +3,8 @@ package com.example.srisri.doodle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.database.DataSnapshot;
@@ -20,41 +22,25 @@ public class CustomRegistration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_registration);
-        HashMap<String, Object> user = new HashMap<>();
-        user.put("name", "Preston Giorgianni");
-        user.put("email", "pgior001@ucr.edu");
-        user.put("password", 12345);
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference ref = database.getReference("Users");
-        ref.child("1").setValue(user);
-        Query query = ref.orderByChild("email");
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+        Button register = (Button) findViewById(R.id.registration_complete_button);
+        register.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if(dataSnapshot != null) {
-                    Log.v("user", dataSnapshot.getValue().toString());
-                } else {
-                    Log.v("user", "none");
-                }
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
+            public void onClick(View view) {
+                createAccount();
             }
         });
-
     }
 
     public void createAccount() {
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference ref = database.getReference("Users");
-        Query query = ref.orderByChild("email").equalTo(((EditText)findViewById(R.id.registration_name)).getText().toString());
+        Query query = ref.orderByChild("email").equalTo(((EditText)findViewById(R.id.registration_email)).getText().toString());
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if(dataSnapshot.getChildrenCount() != 0){
                     //throw null because user found with email
-                    Log.v("register", dataSnapshot.getChildren().iterator().next().getKey());
+                    Log.v("register", "error email already in use");
                 } else {
                     Query id = ref.orderByKey().limitToLast(1);
                     id.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -62,13 +48,10 @@ public class CustomRegistration extends AppCompatActivity {
                         public void onDataChange(DataSnapshot dataSnapshot) {
                             Log.v("register", dataSnapshot.getChildren().iterator().next().getKey());
                             Log.v("register", String.valueOf(Integer.parseInt(dataSnapshot.getChildren().iterator().next().getKey()) + 1));
-//                                User input = new User(((EditText)findViewById(R.id.Password_Register_Input)).getText().toString(),
-//                                        ((EditText)findViewById(R.id.Name_Register_input)).getText().toString(),
-//                                        ((EditText)findViewById(R.id.Email_Register_Input)).getText().toString());
                             HashMap<String, Object> user = new HashMap<>();
-//                            user.put("name", ((EditText)findViewById(R.id.Name_Register_input)).getText().toString());
-//                            user.put("email", ((EditText)findViewById(R.id.Email_Register_Input)).getText().toString());
-//                            user.put("password", ((EditText)findViewById(R.id.Password_Register_Input)).getText().toString());
+                            user.put("name", ((EditText)findViewById(R.id.registration_name)).getText().toString());
+                            user.put("email", ((EditText)findViewById(R.id.registration_email)).getText().toString());
+                            user.put("password", ((EditText)findViewById(R.id.registration_password)).getText().toString());
                             ref.child(String.valueOf(Integer.parseInt(dataSnapshot.getChildren().iterator().next().getKey()) + 1)).setValue(user);
                             finish();
                         }
@@ -86,5 +69,15 @@ public class CustomRegistration extends AppCompatActivity {
 
             }
         });
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
     }
 }
