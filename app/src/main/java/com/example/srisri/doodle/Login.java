7a73af0badf6fd066c51ed3226e8a0458039e7fb
Srisri Gokanapudy <sgoka001@ucr.edu>
2018-01-30 16:10:08 -0800

    register page connected to login

--- app/src/main/java/com/example/srisri/doodle/Login.java
+++ app/src/main/java/com/example/srisri/doodle/Login.java
@@ -2,6 +2,14 @@ package com.example.srisri.doodle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +17,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void registerPage(View view) {
+
+        Intent intent = new Intent(this, CustomRegistration.class);
+        startActivity(intent);
     }
 }
