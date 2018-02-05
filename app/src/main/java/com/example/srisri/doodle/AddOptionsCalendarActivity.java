5061f7c8405f4e70f0e8b1763a1aa4e6f6a981d2
TrevorSalazar <tsala001@ucr.edu>
2018-02-05 06:32:30 -0800

    Creating options calendar files

---
+++ app/src/main/java/com/example/srisri/doodle/AddOptionsCalendarActivity.java
@@ -0,0 +1,13 @@
+package com.example.srisri.doodle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddOptionsCalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_options_calendar);
+    }
+}
