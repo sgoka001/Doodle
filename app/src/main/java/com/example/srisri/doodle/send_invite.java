f3dc3055ce3396be64e4580f53a9a88c3f78dac5
pgior001 <pgior001@ucr.edu>
2018-02-06 12:57:45 -0800

    created send_invite page

---
+++ app/src/main/java/com/example/srisri/doodle/send_invite.java
@@ -0,0 +1,13 @@
+package com.example.srisri.doodle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class send_invite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_invite);
+    }
+}
