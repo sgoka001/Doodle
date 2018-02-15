5f6a02b216c34fcbe189c5822e00ebdc203ac96d
TrevorSalazar <tsala001@ucr.edu>
2018-02-15 09:15:29 -0800

    Added spinner objects in event activity to add all possible options for time of day

--- app/src/main/java/com/example/srisri/doodle/NewEvent.java
+++ app/src/main/java/com/example/srisri/doodle/NewEvent.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +31,22 @@ public class NewEvent extends AppCompatActivity {
         });
         List<String> selected_dates = new ArrayList<>();
         selected_dates = (ArrayList<String>)getIntent().getSerializableExtra("SelectedDates");
+
+        Spinner dropdown = findViewById(R.id.spinner);
+        String[] items = new String[]{"1:", "2:", "3:", "4:", "5:", "6:",
+                "7:", "8:", "9:", "10:","11:", "12:"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        Spinner dropdown2 = findViewById(R.id.spinner2);
+        String[] items2 = new String[]{"00", "15", "30", "45"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown2.setAdapter(adapter2);
+
+        Spinner dropdown3 = findViewById(R.id.spinner3);
+        String[] items3 = new String[]{"AM", "PM"};
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown2.setAdapter(adapter3);
 
         Button evSettings = findViewById(R.id.button4);
         evSettings.setOnClickListener(new View.OnClickListener() {
