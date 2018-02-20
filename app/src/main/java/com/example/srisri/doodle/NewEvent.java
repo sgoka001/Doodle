bcd742c2a4ca256f19e8daa07f27f3d22eba2d9b
TrevorSalazar <tsala001@ucr.edu>
2018-02-20 13:56:58 -0800

    Working on pushing data to Firebase

--- app/src/main/java/com/example/srisri/doodle/NewEvent.java
+++ app/src/main/java/com/example/srisri/doodle/NewEvent.java
@@ -10,13 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class NewEvent extends AppCompatActivity {
 
-    @Override
-
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event);
@@ -29,25 +31,59 @@ public class NewEvent extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        List<String> selected_dates = new ArrayList<>();
-        selected_dates = (ArrayList<String>)getIntent().getSerializableExtra("SelectedDates");
-
-        Spinner dropdown = findViewById(R.id.spinner);
+        final Spinner dropdown = findViewById(R.id.spinner);
         String[] items = new String[]{"1:", "2:", "3:", "4:", "5:", "6:",
                 "7:", "8:", "9:", "10:","11:", "12:"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
 
-        Spinner dropdown2 = findViewById(R.id.spinner2);
+        final Spinner dropdown2 = findViewById(R.id.spinner2);
         String[] items2 = new String[]{"00", "15", "30", "45"};
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown2.setAdapter(adapter2);
 
-        Spinner dropdown3 = findViewById(R.id.spinner3);
+        final Spinner dropdown3 = findViewById(R.id.spinner3);
         String[] items3 = new String[]{"AM", "PM"};
         ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        dropdown2.setAdapter(adapter3);
+        dropdown3.setAdapter(adapter3);
 
+        final List<String> selected_times = new ArrayList<String>();
+        Button evAddOptionDT = findViewById(R.id.button5);
+
+        evAddOptionDT.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                String spinnertext = dropdown.getSelectedItem().toString() +
+                        dropdown2.getSelectedItem().toString() + dropdown3.getSelectedItem().toString();
+                selected_times.add(spinnertext);
+            }
+        });
+
+        Button evFinish = findViewById(R.id.button3);
+        evFinish.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                List<String> selected_dates;
+                selected_dates = getIntent().getStringArrayListExtra("SelectedDates");
+                int eventChoices = selected_dates.size();
+                int i;
+                for(i = 0; i < eventChoices; ++i) {
+                    //DatabaseReference myRef = database.getReference("Events/" + eventID.toString() + "/choices/" + i);
+                    //myRef.setValue(selected_dates.get(i));
+                }
+                for(int j = 0; j < selected_times.size(); ++j) {
+                    //DatabaseReference myRef = database.getReference("Events/" + eventID.toString() + "/choices/" + i);
+                    //myRef.setValue(selected_times.get(j));
+                    i = i + 1;
+                }
+                EditText evTitle = findViewById(R.id.editText);
+                String title = evTitle.getText().toString();
+                //DatabaseReference myRef = database.getReference("Events/" + eventID.toString() + "/title/");
+                //myRef.setValue(title);
+                EditText evNote = findViewById(R.id.editText2);
+                String note = evNote.getText().toString();
+                //myRef = database.getReference("Events/" + eventID.toString() + "/note/");
+                //myRef.setValue(note);
+            }
+        });
         Button evSettings = findViewById(R.id.button4);
         evSettings.setOnClickListener(new View.OnClickListener() {
             @Override
